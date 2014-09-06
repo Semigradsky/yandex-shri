305c6f8df9a061b7a5378cf3f5c61ccb2361ee7c
semigradsky <semigradskyd@gmail.com>
2014-09-06 22:05:05 +0300

    Задание "Dots": пофикшен полет точек и изменение цвета. Теперь цвет изменяется так, как указано в задании, а полет без изменения траектории осуществляется только на небольшое заданное расстояние.

--- Register/Dots/main.js
+++ Register/Dots/main.js
@@ -1,13 +1,19 @@
 (function() {
 
-    init();
-
-    var DEFAULT_CHANGE_COLOR_DURATION = 2000,
+    var COUNT_CIRCLES = 30,
+        DEFAULT_CHANGE_COLOR_DURATION = 2000,
         MOTION_DURATION = 1000,
+        MOTION_DISTANCE = 100,
         CIRCLE_DIAMETER = 20;
 
-    var circles = d3.selectAll('div');
-
+    init();
+    
+    var circles = d3.selectAll('div'),
+        minLeftPos = 0,
+        maxLeftPos = document.body.clientWidth - CIRCLE_DIAMETER,
+        minTopPos = 0,
+        maxTopPos = document.body.clientHeight - CIRCLE_DIAMETER;
+    
     circles.each(function() {
         var circle = d3.select(this);
         animateMotion(circle);
@@ -15,10 +21,14 @@
     });
 
     function animateMotion(circle) {
-        var newLeftPos = document.body.clientWidth * Math.random() - CIRCLE_DIAMETER,
-            newTopPos = document.body.clientHeight * Math.random() - CIRCLE_DIAMETER;
-        newLeftPos = newLeftPos < 0 ? 0 : newLeftPos;
-        newTopPos = newTopPos < 0 ? 0 : newTopPos;
+        var cos = Math.random() * (Math.random() > 0.5 ? 1 : -1),
+            sin = Math.sqrt(1 - cos*cos) * (Math.random() > 0.5 ? 1 : -1),
+            newLeftPos = parseInt(circle.style('left')) + MOTION_DISTANCE * cos,
+            newTopPos = parseInt(circle.style('top')) + MOTION_DISTANCE * sin;
+        
+        newLeftPos = Math.max(minLeftPos, Math.min(maxLeftPos, newLeftPos));
+        newTopPos = Math.max(minTopPos, Math.min(maxTopPos, newTopPos));
+
         circle
             .transition()
             .duration(MOTION_DURATION)
@@ -29,17 +39,19 @@
     }
 
     function animateChangeColor(circle) {
-        var duration = DEFAULT_CHANGE_COLOR_DURATION * Math.random();
+        var duration = DEFAULT_CHANGE_COLOR_DURATION * Math.random(),
+            newColor = circle.style('border-color') === 'rgb(255, 0, 0)' ? 'rgb(255, 255, 0)' : 'rgb(255, 0, 0)';
+        
         circle
             .transition()
             .duration(duration)
-            .ease('elastic')
-            .style('border-color', 'rgb(255, ' + Math.ceil(Math.random() * 255) + ', 0)');
+            .ease('linear')
+            .style('border-color', newColor);
         setTimeout(function() { animateChangeColor(circle); }, duration);
     }
 
     function init() {
-        createCircles(100);
+        createCircles(COUNT_CIRCLES);
 
         function createCircles(count) {
             for (var i = count; i--;) {
