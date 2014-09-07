c70e60b18881fa54c91672e439e2e45e5a07665a
semigradsky <semigradskyd@gmail.com>
2014-09-07 22:27:56 +0300

    Задание "Dots": изменена инициализация и изменение координат точек, теперь точки своими координатами визуализируют некоторые данные

--- Register/Dots/main.js
+++ Register/Dots/main.js
@@ -1,18 +1,20 @@
 (function() {
 
-    var COUNT_CIRCLES = 30,
-        DEFAULT_CHANGE_COLOR_DURATION = 2000,
-        MOTION_DURATION = 1000,
-        MOTION_DISTANCE = 100,
-        CIRCLE_DIAMETER = 20;
-
-    init();
+    var COUNT_CIRCLES = 20, // count
+        DEFAULT_CHANGE_COLOR_DURATION = 2000, // ms
+        MOTION_DURATION = 100, // ms
+        MOTION_DISTANCE = 20, // px
+        CIRCLE_DIAMETER = 20; // px
     
-    var circles = d3.selectAll('div'),
-        minLeftPos = 0,
+    var minLeftPos = 0,
         maxLeftPos = document.body.clientWidth - CIRCLE_DIAMETER,
         minTopPos = 0,
         maxTopPos = document.body.clientHeight - CIRCLE_DIAMETER;
+
+    var body = d3.select('body'),
+        data = generateData(),
+        circles = body.selectAll('div').data(data).enter().append('div');
+    applyData(circles);
     
     circles.each(function() {
         var circle = d3.select(this);
@@ -21,21 +23,40 @@
     });
 
     function animateMotion(circle) {
+        var newData = generateNewData(circle.data()[0]);
+        circle.datum(newData);
+
+        var motionCircle = circle
+            .transition()
+            .duration(MOTION_DURATION)
+            .ease('linear');
+        applyData(motionCircle);
+        setTimeout(function() { animateMotion(circle); }, MOTION_DURATION);
+    }
+    
+    function generateNewData(oldData) {
         var cos = Math.random() * (Math.random() > 0.5 ? 1 : -1),
             sin = Math.sqrt(1 - cos*cos) * (Math.random() > 0.5 ? 1 : -1),
-            newLeftPos = parseInt(circle.style('left')) + MOTION_DISTANCE * cos,
-            newTopPos = parseInt(circle.style('top')) + MOTION_DISTANCE * sin;
+            newLeftPos = Math.round(oldData.x + MOTION_DISTANCE * cos),
+            newTopPos = Math.round(oldData.y + MOTION_DISTANCE * sin);
         
         newLeftPos = Math.max(minLeftPos, Math.min(maxLeftPos, newLeftPos));
         newTopPos = Math.max(minTopPos, Math.min(maxTopPos, newTopPos));
-
-        circle
-            .transition()
-            .duration(MOTION_DURATION)
-            .ease('linear')
-            .style('left', newLeftPos + 'px')
-            .style('top', newTopPos + 'px');
-        setTimeout(function() { animateMotion(circle); }, MOTION_DURATION);
+        
+        return {
+            x: newLeftPos,
+            y: newTopPos
+        };
+    }
+    
+    function applyData(el) {
+        el.style('left', function(data) {
+            return data.x + 'px';
+        })
+        .style('top', function(data) {
+            return data.y + 'px';
+        });
+        return el;
     }
 
     function animateChangeColor(circle) {
@@ -49,16 +70,20 @@
             .style('border-color', newColor);
         setTimeout(function() { animateChangeColor(circle); }, duration);
     }
-
-    function init() {
-        createCircles(COUNT_CIRCLES);
-
-        function createCircles(count) {
-            for (var i = count; i--;) {
-                var circle = document.createElement('div');
-                document.body.appendChild(circle);
-            }
+    
+    function generateData() {
+        var data = [];
+        for (var i = COUNT_CIRCLES; i--;) {
+            data.push({
+                x: random(minLeftPos, maxLeftPos),
+                y: random(minTopPos, maxTopPos)
+            });
         }
+        return data;
+    }
+    
+    function random(lower, upper) {
+        return Math.round(Math.random() * (upper - lower)) + lower;
     }
 
 })();
